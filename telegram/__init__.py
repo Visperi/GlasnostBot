4c0de724f51dfede430cd1605b130392f9a6913d
Visperi <niksuposti@gmail.com>
2022-11-22 15:15:38 +0200

    Import useful classes from library

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,2 +1,14 @@
+from .chat import *
 from .client import *
+from .contact import *
+from .document import *
+from .game import *
+from .inline_query import *
+from .location import *
+from .message import *
+from .payments import *
+from .poll import *
+from .update import *
+from .user import *
+
 from . import utils
