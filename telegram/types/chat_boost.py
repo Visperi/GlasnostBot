d944bc43109d6fb8d0c4538cfaab0d913cb2afcb
Visperi <niko.matasaho@gmail.com>
2025-11-04 16:55:33 +0200

    Update Telegram type class Update

---
+++ telegram/types/chat_boost.py
@@ -0,0 +1,43 @@
+from typing_extensions import TypedDict, NotRequired
+
+from .chat import Chat
+from .user import User
+
+
+# TODO: Combine/refactor the boost classes
+class ChatBoostSource(TypedDict):
+    source: str
+
+
+class ChatBoostSourcePremium(ChatBoostSource):
+    user: User
+
+
+class ChatBoostSourceGiftCode(ChatBoostSource):
+    user: User
+
+
+class ChatBoostSourceGiveaway(ChatBoostSource):
+    giveaway_message_id: int
+    user: NotRequired[User]
+    prize_star_count: NotRequired[int]
+    is_unclaimed: NotRequired[bool]
+
+
+class ChatBoost(TypedDict):
+    boost_id: str
+    add_date: int
+    expiration_date: int
+    source: ChatBoostSource
+
+
+class ChatBoostRemoved(TypedDict):
+    chat: Chat
+    boost_id: str
+    remove_date: int
+    source: ChatBoostSource
+
+
+class ChatBoostUpdated(TypedDict):
+    chat: Chat
+    boost: ChatBoost
---
+++ telegram/types/reaction.py
@@ -0,0 +1,22 @@
+from typing_extensions import TypedDict
+
+
+class ReactionType(TypedDict):
+    type: str
+
+
+class ReactionTypeEmoji(ReactionType):
+    emoji: str
+
+
+class ReactionTypeCustomEmoji(ReactionType):
+    custom_emoji_id: str
+
+
+class ReactionTypePaid(ReactionType):
+    type: str
+
+
+class ReactionCount(TypedDict):
+    type: ReactionType
+    total_count: int
