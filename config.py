ee54a66edcf1fc254c8d99de6f11d78d3e818e07
Visperi <niko.matasaho@gmail.com>
2025-11-13 09:47:15 +0200

    Add docstrings

--- config.py
+++ config.py
@@ -34,10 +34,20 @@ class __ConfigSection:
     __slots__ = ()
 
     def __init__(self, section_dict: dict):
+        """
+        A base class for all sections in a TOML file.
+
+        :param section_dict: Config section as a dictionary.
+        """
         for key, value in section_dict.items():
             setattr(self, key, value)
 
     def as_dict(self) -> dict:
+        """
+        Get a configuration section as a dictionary.
+
+        :return: A config section as a dictionary. This is the same format as loading a config TOML would give.
+        """
         d = {}
         for attr_name in self.__slots__:
             attr_value = getattr(self, attr_name)
@@ -50,6 +60,11 @@ class __ConfigSection:
 
     @classmethod
     def generate_default(cls):
+        """
+        Generate a config section object with default values.
+
+        :raises NotImplementedError: If ``generate_default`` method is not implemented in the section class.
+        """
         raise NotImplementedError(f"generate_default method not implemented in {cls.__name__}")
 
 class _Preferences(__ConfigSection):
@@ -63,6 +78,11 @@ class _Preferences(__ConfigSection):
     )
 
     def __init__(self, preferences_dict: dict):
+        """
+        An object representing preferences section in a TOML file.
+
+        :param preferences_dict: A preferences section as a dictionary.
+        """
         super().__init__(preferences_dict)
 
     @classmethod
@@ -83,6 +103,11 @@ class _BotSettings(__ConfigSection):
     )
 
     def __init__(self, settings_dict: dict):
+        """
+        An object representing bot_settings section in a TOML file.
+
+        :param settings_dict: A bot_settings section as a dictionary.
+        """
         super().__init__(settings_dict)
 
     @classmethod
@@ -100,6 +125,11 @@ class _ChannelIds(__ConfigSection):
     )
 
     def __init__(self, channel_ids_dict: dict):
+        """
+        An object representing channel_ids section in a TOML file.
+
+        :param channel_ids_dict: A channel_ids section as a dictionary.
+        """
         super().__init__(channel_ids_dict)
 
     @classmethod
@@ -116,6 +146,11 @@ class _Credentials(__ConfigSection):
     )
 
     def __init__(self, credentials_dict: dict):
+        """
+        An object representing credentials section in a TOML file.
+
+        :param credentials_dict: A credentials section as a dictionary.
+        """
         super().__init__(credentials_dict)
 
     @classmethod
@@ -131,6 +166,11 @@ class _General(__ConfigSection):
     )
 
     def __init__(self, general_dict: dict):
+        """
+        An object representing general section in a TOML file.
+
+        :param general_dict: A general section as a dictionary.
+        """
         super().__init__(general_dict)
 
     @classmethod
@@ -141,6 +181,12 @@ class _General(__ConfigSection):
 class Config:
 
     def __init__(self, config_path: Optional[str] = None):
+        """
+        An object representing a TOML configuration file.
+
+        :param config_path: Path to an existing TOML configuration file. If omitted, a configuration load a file later
+                            with method ``load`` or assign the sections.
+        """
         self.config_path = config_path
         self.general: _General = Missing
         self.credentials: _Credentials = Missing
@@ -153,6 +199,11 @@ class Config:
 
     @classmethod
     def with_default_values(cls):
+        """
+        Initialize a ``Config`` object with default values.
+
+        :return: ``Config`` object with default configuration.
+        """
         obj = cls()
         obj.general = _General.generate_default()
         obj.credentials = _Credentials.generate_default()
@@ -162,7 +213,18 @@ class Config:
 
         return obj
 
+    def __repr__(self):
+        return str(self.as_dict())
+
+    def __str__(self):
+        return toml.dumps(self.as_dict())
+
     def load(self, config_file: Optional[str] = None):
+        """
+        Load configuration from an existing TOML file.
+
+        :param config_file: Path to the TOML file.
+        """
         if config_file:
             config = toml.load(config_file)
         else:
@@ -175,10 +237,20 @@ class Config:
         self.preferences = _Preferences(config["preferences"])
 
     def save(self, output_file: str):
+        """
+        Save the configuration to a file.
+
+        :param output_file: Path to the file.
+        """
         with open(output_file, "w", encoding="utf-8") as out_file:
             toml.dump(self.as_dict(), out_file)
 
     def as_dict(self) -> dict:
+        """
+        Get the configuration as a dictionary.
+
+        :return: A dictionary of current values. This is the same format as loading a config TOML would give.
+        """
         d = {}
 
         sections = self.__dict__.copy()
@@ -190,12 +262,6 @@ class Config:
                 d[section_name] = section_value
 
         return d
-
-    def __repr__(self):
-        return str(self.as_dict())
-
-    def __str__(self):
-        return toml.dumps(self.as_dict())
 
 
 if __name__ == "__main__":
