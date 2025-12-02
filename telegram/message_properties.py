84306eb49c83b92e89bd782ef8a4fb5bb7b65768
Visperi <niko.matasaho@gmail.com>
2025-12-02 09:11:38 +0200

    Add TextQuote docstrings

--- telegram/message_properties.py
+++ telegram/message_properties.py
@@ -367,11 +367,29 @@ class TextQuote:
     )
 
     def __init__(self, payload: TextQuotePayload):
-        self.text: str = payload["text"]
-        self.entities: List[MessageEntity] = [MessageEntity(e) for e in payload.get("entities", [])]
-        self.position = payload["position"]
-        self.is_manual = payload.get("is_manual", False)
+        """
+        Represents a quoted part in a message that quotes and replies another message.
 
+        :param payload: ``TextQuote`` dictionary received from Telegram API.
+        """
+        self.text: str = payload["text"]
+        """
+        Quoted part of the replied message.
+        """
+        self.entities: List[MessageEntity] = [MessageEntity(e) for e in payload.get("entities", [])]
+        """
+        List of message entities in the quote.
+        """
+        self.position = payload["position"]
+        """
+        Approximate position on the quote in the original message in UTF-16 code units.
+        """
+        self.is_manual = payload.get("is_manual", False)
+        """
+        True if the quote was manually selected by user. Otherwise the quote was added by Telegram servers.
+        """
+
+    # TODO: Relocate the markdown algorithm to common place where it can be called where applicable
     def markdown(self, make_urls_to_hyperlinks: bool = True) -> str:
         sorted_entities = sorted(self.entities, key=lambda e: e.offset)
         grouped_entities = {}
